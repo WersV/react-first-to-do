384e97b16975d2ed1b331eda2ab7151758d5d8d3
Przemyslaw Botwina <przemek.botwina@o2.pl>
2023-11-22 20:57:45 +0100

    x

--- src/Info/Info.jsx
+++ src/Info/Info.jsx
@@ -5,7 +5,7 @@ export function Info() {
     <>
       <div className={styled.infoWrapper}>
         <h1 className={styled.header1}>Do zrobienia</h1>
-        <h2 className={styled.header2}>2 zadania</h2>
+        {/* <h2 className={styled.header2}>2 zadania</h2> */}
       </div>
     </>
   );
